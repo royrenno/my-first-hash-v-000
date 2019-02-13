c521944c467aef533f5cc344e6b3c03e2fca8720
roy renno <royrenno@yahoo.com>
2019-02-13 01:35:30 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -21,15 +21,10 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  shipping_manifest["muskets"]=2             
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
+  shipping_manifest["muskets"]=2  
+  shipping_manifest["gun powder"]=4       
+  
 
   # return the shipping_manifest hash below
-
+puts shipping_manifest
 end
