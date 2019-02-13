9a5325f75d8ab27e73bedd5e2035acc5329ca371
roy renno <royrenno@yahoo.com>
2019-02-13 01:31:32 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -12,10 +12,8 @@ def retrieval
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-                 shipping_manifest[ "oil paintings"]
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
-end
+   shipping_manifest[ "oil paintings"]
+  end
 
 def adding
   shipping_manifest = {
@@ -23,6 +21,7 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
+  shipping_manifest["muskets"] =2             
   # add 2 muskets to the shipping_manifest hash below
 
 
